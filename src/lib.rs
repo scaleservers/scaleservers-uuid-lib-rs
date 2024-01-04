953f6ce380098c987f92ea88885e2e3580028174
kit-scaleservers <154639387+kit-scaleservers@users.noreply.github.com>
2024-01-04 19:30:29 -0400

    Update LICENSE; initial library scaffolding

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+// Copyright (c) Scaleservers LLC
+// Licensed under the MIT License
+
+mod uuid;
+pub use uuid::Uuid;
---
+++ src/uuid.rs
@@ -0,0 +1,5 @@
+// Copyright (c) Scaleservers LLC
+// Licensed under the MIT License
+
+pub struct Uuid {
+}
